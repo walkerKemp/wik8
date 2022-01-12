d0530347f4fb671f43704be760f380caa3238762
walkerKemp <attackingsuitcase@gmail.com>
2022-01-12 02:44:28 -0500

    Rewrote compiler for the new spec sheet

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -3,10 +3,8 @@
 import re
 from os.path import exists
 from sys import argv
-
-DST = 0
-NUM = 1
-OFF = 2
+from os import _exit
+from time import time
 
 class TYPES:
   DST = 0
@@ -14,151 +12,204 @@ class TYPES:
   OFF = 2
 
 instructions = {
-  # inst: (num_args, op_code)
-  "nop": ([], 0x10),
-  "zer": ([TYPES.DST], 0x11),
-  "mvi": ([TYPES.NUM, TYPES.DST], 0x12),
-  "mov": ([TYPES.DST, TYPES.DST], 0x13),
-  "adi": ([TYPES.NUM], 0x14),
-  "add": ([TYPES.DST], 0x15),
-  "sbi": ([TYPES.NUM], 0x16),
-  "sub": ([TYPES.DST], 0x17),
-  "swp": ([], 0x18),
-  "sav": ([], 0x19),
-  "jmp": ([TYPES.OFF], 0x1A),
-  "jez": ([TYPES.OFF], 0x1B),
-  "jnz": ([TYPES.OFF], 0x1C),
-  "jgz": ([TYPES.OFF], 0x1D),
-  "cei": ([TYPES.NUM], 0x1E),
-  "cni": ([TYPES.NUM], 0x1F),
-  "cgi": ([TYPES.NUM], 0x20),
-  "cli": ([TYPES.NUM], 0x21),
-  "cet": ([TYPES.DST], 0x22),
-  "cnt": ([TYPES.DST], 0x23),
-  "cgt": ([TYPES.DST], 0x24),
-  "clt": ([TYPES.DST], 0x25),
-  "psh": ([], 0x26),
-  "pop": ([], 0x27),
-  "inc": ([], 0x28),
-  "dec": ([], 0x29),
-  "pnc": ([], 0x2A),
-  "fnc": ([TYPES.OFF], 0x2B),
-  "ret": ([], 0x2C),
+  # inst: ([arg_list], op_code)
+  "nop": ([], 0x20),
+  "zer": ([TYPES.DST], 0x21),
+  "mvi": ([TYPES.DST, TYPES.NUM], 0x22),
+  "mov": ([TYPES.DST, TYPES.DST], 0x23),
+  "adi": ([TYPES.NUM], 0x24),
+  "add": ([TYPES.DST], 0x25),
+  "sbi": ([TYPES.NUM], 0x26),
+  "sub": ([TYPES.DST], 0x27),
+  "swp": ([], 0x28),
+  "sav": ([], 0x29),
+  "jmp": ([TYPES.OFF], 0x2a),
+  "jez": ([TYPES.OFF], 0x2b),
+  "jnz": ([TYPES.OFF], 0x2c),
+  "jgz": ([TYPES.OFF], 0x2d),
+  "jlz": ([TYPES.OFF], 0x2e),
+  "cei": ([TYPES.NUM], 0x2f),
+  "cni": ([TYPES.NUM], 0x30),
+  "cgi": ([TYPES.NUM], 0x31),
+  "cli": ([TYPES.NUM], 0x32),
+  "cet": ([TYPES.DST], 0x33),
+  "cnt": ([TYPES.DST], 0x34),
+  "cgt": ([TYPES.DST], 0x35),
+  "clt": ([TYPES.DST], 0x36),
+  "psh": ([TYPES.DST], 0x37),
+  "pop": ([TYPES.DST], 0x38),
+  "inc": ([], 0x39),
+  "dec": ([], 0x3a),
+  "fnc": ([TYPES.OFF], 0x3b),
+  "ret": ([], 0x3c),
+  "rsi": ([TYPES.NUM, TYPES.NUM], 0x3d),
+  "rsd": ([TYPES.NUM, TYPES.DST], 0x3e),
+  "rai": ([TYPES.DST, TYPES.NUM], 0x3f),
+  "rad": ([TYPES.DST, TYPES.DST], 0x3f),
+  "kil": ([], 0x41)
 }
 
 registers = {
-  "pcc": 0x03,
-  "acc": 0x04,
-  "bak": 0x05,
-  "stk": 0x06,
-  "lst": 0x07,
-  "fl0": 0x08,
-  "fl1": 0x09,
+  "pcc": 0x10,
+  "acc": 0x11,
+  "bak": 0x12,
+  "stk": 0x13,
+  "fl0": 0x14,
+  "fl1": 0x15,
 }
 
-def is_valid_label(lab):
-  result = re.findall("\A[a-z][a-z0-9]*:$", lab)
-  return len(result) == 1
+def print_err_exit(message):
+  print(message)
+  _exit(1)
+
+def print_warn_exit(message):
+  print(message)
 
 class Lexer:
-  def __init__(self, code):
-    self.code = code.split(" ")
-    self.pointer = -1
-    self.max_pointer = len(self.code) - 1
-    self.current_word = ""
+  def __init__(self, file_name, code):
+    self.file_name = file_name
+    self.code = code
+    self.tokens = []
+    self.pointer = 0
+    self.max_pointer = 0
     self.labels = {}
     self.result = []
 
+  def compile(self):
+    self.tokenize()
+    self.generate_labels()
     self.parse()
+    return self.result
+
+  def tokenize(self):
+    for i, line in enumerate(self.code.split("\n")):
+      pointer_in_line = 0
+      while pointer_in_line < len(line):
+        word = ""
+        while line[pointer_in_line].isspace():
+          pointer_in_line += 1
+        while pointer_in_line < len(line) and not line[pointer_in_line].isspace():
+          word += line[pointer_in_line]
+          pointer_in_line += 1
+        self.tokens.append((i, pointer_in_line - len(word), word))
+    self.max_pointer = len(self.tokens) - 1
 
   def parse(self):
-    self.generate_labels()
-
     while self.peek(1) != "\0":
-      self.next()
+      x, y, tok = self.next()
 
-      if instructions.get(self.current_word, None) != None:
-        arg_types, op_code = instructions[self.current_word]
-        self.result.append(self.convert_byte_size(op_code))
+      if tok in instructions.keys():
+        args, op_code = instructions[tok]
 
-        for i in arg_types:
+        self.result.append(op_code.to_bytes(1, "big"))
+
+        for i in args:
+          if i == TYPES.NUM:
+            self.do_num()
           if i == TYPES.DST:
             self.do_dst()
-          elif i == TYPES.NUM:
-            self.do_num()
-          elif i == TYPES.OFF:
+          if i == TYPES.OFF:
             self.do_off()
-
-  def convert_byte_size(self, num):
-    res = num.to_bytes(4, "big")
-    print(f" [!] Converting {num} to {res}")
-    return res
-
-  def generate_labels(self):
-    for i, v in enumerate(self.code):
-      if is_valid_label(v):
-        self.labels[v.replace(":", "")] = i
-        self.code[i] = "nop"
-
-  def do_dst(self):
-    reg = self.next()
-    assert reg != "\0", f"Unexpected end of file at {self.pointer}"
-    assert registers.get(reg, None) != None, f"Invalid reg {reg}"
-    self.result.append(self.convert_byte_size(registers[reg]))
-
-  def do_num(self):
-    num = self.next()
-    assert num != "\0", f"Unexpected end of file at {self.pointer}"
-    assert num.isdigit(), f"Invalid number {num} as {self.pointer}"
-    self.result.append(self.convert_byte_size(int(num)))
-
-  def do_off(self):
-    lab = self.next()
-    assert self.labels.get(lab, None) != None, f"Invalid label {lab}"
-    self.result.append(self.convert_byte_size(self.labels[lab]))
+      elif self.is_valid_label(tok):
+        pass
+      else:
+        print_err_exit(f"./{self.file_name}:{x}:{y}: Unexpected token {tok}")
 
   def next(self):
-    if self.pointer + 1 > self.max_pointer:
-      self.current_word = "\0"
-      return self.current_word
+    if self.pointer > self.max_pointer:
+      return "\0"
 
+    token = self.tokens[self.pointer]
     self.pointer += 1
-    self.current_word = self.code[self.pointer]
-    return self.current_word
+    return token
 
   def peek(self, offset):
     if self.pointer + offset > self.max_pointer:
       return "\0"
 
-    return self.code[self.pointer + offset]
+    token = self.tokens[self.pointer]
+    return token
 
-def get_code_file(file_path):
-  assert exists(file_path), f" [-] File not found: {file_path}"
+  def do_num(self):
+    tok = self.next()
+    if tok == "\0":
+      print_err_exit(f"./{self.file_name}: Expected number, found EOF")
 
-  data = ""
-  with open(file_path, "r") as f:
-    data = f.read()
-  data = data.replace("\n", " ")
+    x, y, num = tok
 
-  return data
+    if not num.isdigit():
+      print_err_exit(f"./{self.file_name}:{x}:{y}: Expected number, found {num}")
 
-def write_bytes_file(file_path, code_bytes):
-  with open(file_path, "wb") as f:
-    for byte in code_bytes:
-      f.write(byte)
+    if int(num) > 2147483647:
+      print_err_exit(f"./{self.file_name}:{x}:{y}: Number {num} is too big, must be within i32")
+
+    if int(num) < -2147483648:
+      print_err_exit(f"./{self.file_name}:{x}:{y}: Number {num} is too small, must be within i32")
+
+    self.result.append(int(num).to_bytes(4, "big"))
+
+  def do_dst(self):
+    tok = self.next()
+    if tok == "\0":
+      print_err_exit(f"./{self.file_name}: Expected register, found EOF")
+
+    x, y, dst = tok
+
+    if dst not in registers.keys():
+      print_err_exit(f"./{self.file_name}:{x}:{y}: Expected register, found {dst}")
+
+    self.result.append(registers[dst].to_bytes(1, "big"))
+
+  def do_off(self):
+    tok = self.next()
+    if tok == "\0":
+      print_err_exit(f"./{self.file_name}: Expected label, found EOF")
+
+    x, y, lab = tok
+
+    if lab not in self.labels.keys():
+      print_err_exit(f"./{self.file_name}:{x}:{y}: Unknown label: {lab}")
+
+    self.result.append(self.labels[lab].to_bytes(4, "big"))
+
+  def generate_labels(self):
+    for i, tok in enumerate(self.tokens):
+      if self.is_valid_label(tok[2]):
+        if tok[2].replace(":", "") in self.labels.keys():
+          print_warn_exit(f"./{self.file_name}:{x}:{y}: Warning: Redefinition of label{tok[1]}")
+
+        self.labels[tok[2].replace(":", "")] = i
+
+  @staticmethod
+  def is_valid_label(lab):
+    result = re.findall("\A[a-z][a-z0-9]*:$", lab)
+    return len(result) == 1
 
 def main():
-  assert len(argv) == 2, f" [-] Invalid number of arguments, is {len(argv) - 1} should be 1"
+  if len(argv) != 2:
+    print("Invalid number of arguments")
+    print_err_exit(f"USAGE: ./{__file__} <file_path>")
 
   file = argv[1]
+  if not exists(file):
+    print_err_exit(f"File not found: {file}")
 
-  code = get_code_file(file)
-  lex = Lexer(code)
-  result = lex.result
+  data = ""
+  with open(file, "r") as f:
+    data = f.read()
 
-  output_file = ".".join(file.split(".")[:-1]) + ".bin"
-  write_bytes_file(output_file, result)
+  lex = Lexer(file, data)
+  lex.compile()
+
+  out_file = f"{''.join(file.split('.')[:-1])}.bin"
+  with open(out_file, "wb") as f:
+    for byte in lex.result:
+      f.write(byte)
 
 if __name__ == "__main__":
+  start = time()
   main()
+  print(f"Compilation successful {round(time() - start, 5)}s")
+
+
 
